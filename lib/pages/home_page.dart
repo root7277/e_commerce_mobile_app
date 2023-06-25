bb3dcef4ec6332469f345b19ddc2a5e2876dc4e1
network7277 <husanjurayev7277@gmail.com>
2023-06-25 10:16:57 +0500

    init

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -139,7 +139,7 @@ class _HomePageState extends State<HomePage> {
                   ),
                 ),
               ],
-              ),
+            ),
            ],
           ),
           Positioned(
